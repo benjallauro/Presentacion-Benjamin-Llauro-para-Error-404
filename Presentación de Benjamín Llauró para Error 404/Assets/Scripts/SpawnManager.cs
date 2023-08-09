183eaebb041e496713548bac5cf273527daa879f
benjaminllauro <benjallauro@gmail.com>
2023-08-09 16:47:58 -0300

    Added multiple-spawning at the same time

--- Presentación de Benjamín Llauró para Error 404/Assets/Scripts/SpawnManager.cs
+++ Presentación de Benjamín Llauró para Error 404/Assets/Scripts/SpawnManager.cs
@@ -37,27 +37,33 @@ public class SpawnManager : MonoBehaviour
     {
         if(timer.Update(Time.deltaTime) && objectPools.Length > 0)
         {
-            int poolToSelect;
-            if (_inTargetEffect)
+            int objectsToSpawn = UnityEngine.Random.Range(_difficulty.minimumObjectsPerSpawn, _difficulty.maximumObjectsPerSpawn);
+
+            for(int i = 0; i < objectsToSpawn; i++)
             {
-                poolToSelect = targetRewardObjectPoolNumber;
-                _currentTargetObjectsDropped++;
-            }
-            else
-                poolToSelect = UnityEngine.Random.Range(0, objectPools.Length);
-            GameObject pooledObject = objectPools[poolToSelect].GetPooledObject().gameObject;
-            pooledObject.transform.position = randomAreaPositioner.RandomizePosition(pooledObject.transform.position);
-            GlobalEventsCaller globalEventsCaller = pooledObject.GetComponent<GlobalEventsCaller>();
-            globalEventsCaller.SetGlobalEvent(globalEventForObjects[poolToSelect]);
-            globalEventsCaller.SetGlobalEvent2(globalOutliveEventForObjects[poolToSelect]);
-            pooledObject.GetComponent<RecycleAfterTime>().StartTimer();
-            timer.StopAndReset();
-            timer.SetTimer(UnityEngine.Random.Range(_difficulty.minimumTimeBetweenSpawns, _difficulty.maximumTimeBetweenSpawns));
-            timer.Start();
-            if (_currentTargetObjectsDropped >= objectsGrantedForClickingTarget)
-            {
-                _inTargetEffect = false;
-                _currentTargetObjectsDropped = 0;
+                int poolToSelect;
+                if (_inTargetEffect)
+                {
+                    poolToSelect = targetRewardObjectPoolNumber;
+                    _currentTargetObjectsDropped++;
+                }
+                else
+                    poolToSelect = UnityEngine.Random.Range(0, objectPools.Length);
+                GameObject pooledObject = objectPools[poolToSelect].GetPooledObject().gameObject;
+                pooledObject.transform.position = randomAreaPositioner.RandomizePosition(pooledObject.transform.position);
+                GlobalEventsCaller globalEventsCaller = pooledObject.GetComponent<GlobalEventsCaller>();
+                globalEventsCaller.SetGlobalEvent(globalEventForObjects[poolToSelect]);
+                globalEventsCaller.SetGlobalEvent2(globalOutliveEventForObjects[poolToSelect]);
+                pooledObject.GetComponent<RecycleAfterTime>().StartTimer();
+                
+                if (_currentTargetObjectsDropped >= objectsGrantedForClickingTarget)
+                {
+                    _inTargetEffect = false;
+                    _currentTargetObjectsDropped = 0;
+                }
+                timer.StopAndReset();
+                timer.SetTimer(UnityEngine.Random.Range(_difficulty.minimumTimeBetweenSpawns, _difficulty.maximumTimeBetweenSpawns));
+                timer.Start();
             }
         }
     }
